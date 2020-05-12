85506de2905496d0bba2b2200700101d346bfb44
juice <jbak@mtech.edu>
2020-05-12 17:06:44 -0600

    fixed little boo boo

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 ln -s ./ssh/config ~/.ssh/config
 ln -s ./bashrc ~/.bashrc
 ln -s ./vim/.vimrc ~/.vimrc
-ln -s ./vim/.vim/ ~/.vim/
+ln -s ./vim/.vim/ ~/.vim
 ln -s ./unison/waiter.prf ~/.unison/waiter.prf
